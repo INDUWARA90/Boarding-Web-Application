f6eb01f8070bc3b949774f8e4090dcb6179ad59b
INDUWARA90 <induwaravishwakantha90@gmail.com>
2025-07-04 06:57:30 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='px-28 mt-4'>
+          <h1>LOGO</h1>
+
+          <nav>
+              <ul className=''>
+                <li>Home</li>
+              </ul>
+          </nav>
+    </div>
+  )
+}
+
+export default Header
